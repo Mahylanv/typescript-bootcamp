9d995d58b102442a71ba15008e87fadbdd0c1c3d
Mahylan <mahylan.veclin@wangarden.com>
2025-02-26 16:27:52 +0100

    EP2

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
-import { creerScore, afficherScore } from "./tuples.js";
-const score = creerScore();
-afficherScore(score);
+import { Direction, vecteurDirection } from "./enum.js";
+console.log("Vecteur Nord:", vecteurDirection(Direction.Nord));
+console.log("Vecteur Sud:", vecteurDirection(Direction.Sud));
+console.log("Vecteur Est:", vecteurDirection(Direction.Est));
+console.log("Vecteur Ouest:", vecteurDirection(Direction.Ouest));
