f7a2a99204bb2baf32c7f14ff31a87205a0061cb
Mahylan <mahylan.veclin@wangarden.com>
2025-02-26 16:25:19 +0100

    EP1

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,3 @@
-import { Rectangle, Cercle } from "./formes.js";
-const monRectangle = new Rectangle(10, 5);
-console.log(`Surface du rectangle: ${monRectangle.calculerSurface()}`);
-const monCercle = new Cercle(7);
-console.log(`Surface du cercle: ${monCercle.calculerSurface()}`);
+import { creerScore, afficherScore } from "./tuples.js";
+const score = creerScore();
+afficherScore(score);
